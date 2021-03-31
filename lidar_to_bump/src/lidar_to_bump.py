95371a46ac94dbe16b0e55195ec5311b3380bebc
Trevor Sherrard <sherrardtr@gmail.com>
2021-03-31 16:18:35 -0400

    lidar_to_bump node mostly working

--- lidar_to_bump/src/lidar_to_bump.py
+++ lidar_to_bump/src/lidar_to_bump.py
@@ -11,12 +11,18 @@
 #              the direction of the "bump"
 
 import rospy
+import math
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
 
 # misc variables
 laserscan_topic = "/trina2_1/base_scan"
-bump_tresh = 0.5 # distanced to be considered a bump hazard
+angle_topic = "/trina2_1/bump_angle"
+bump_thresh = 0.6 # distanced to be considered a bump hazard
 min_bumps = 10   # minimum number of consecutive close points to be considered a bump
+
+# create publisher for angle estimate
+angle_pub = rospy.Publisher(angle_topic, Float64, queue_size=5)
 
 def proc_laserscan(msg):
     """
@@ -31,6 +37,7 @@ def proc_laserscan(msg):
     # loop over points 
     num_consecutive_bumps = 0
     last_point_bump = False
+    total_angle_accum = 0
     for point in points:
         # increment current angle
         start_angle += angle_inc
@@ -40,11 +47,27 @@ def proc_laserscan(msg):
         if(point < bump_thresh):
             last_point_bump = True
             num_consecutive_bumps += 1
+            total_angle_accum += start_angle
+
+            # if we have gone over minimum number of 
+            # consecutive bump points, estimate angle towards
+            # bump from pespective of robot.
+            if(num_consecutive_bumps > min_bumps):
+                angle_est_rad = total_angle_accum / num_consecutive_bumps
+                angle_est_deg = angle_est_rad * (180/math.pi)
+
+                # create new float message
+                angle_msg = Float64()
+                angle_msg.data = angle_est_deg
+
+                # publish new message
+                angle_pub.publish(angle_msg)
         
         # otherwise, set flags and reset number of bumps
         else:
             last_point_bump = False
             num_consecutive_bumps = 0
+            total_angle_accum = 0
         
 
 
