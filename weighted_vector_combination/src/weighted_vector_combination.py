515a647f0d9eea8ad3d273f8d29bc7ff8600db48
Trevor Sherrard <sherrardtr@gmail.com>
2021-04-14 09:26:53 -0400

    initial commit of weighted_vector_combination package

---
+++ weighted_vector_combination/src/weighted_vector_combination.py
@@ -0,0 +1,6 @@
+# Author: Trevor Sherrard
+# Course: Motion Planning (RBE550)
+# Since: 04/14/2020
+# Purpose: This script subscribes to the generated information entropy pose
+#          and adds the constribution of the computed saliency vector as a weighted 
+#          offset. 
