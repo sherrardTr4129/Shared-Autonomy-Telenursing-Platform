e42edbea477eb1726da7aef523ec3b558cfc1428
Albert Enyedy <aenyedy56@gmail.com>
2021-05-12 22:43:59 -0400

    updated documentation for viewpoint attainment nodes

--- camera_motion_algorithms/README.md
+++ camera_motion_algorithms/README.md
@@ -1,2 +1,25 @@
 # Camera Motion Algorithms
 This folder contains all the camera motion algorithms produced for this project (i.e. wrist roll reduction, motion buffer, etc) as ROS packages.
+
+## Stop-to-Fixate Package
+The stop-to-fixate behavior is launched through the stop_to_fixate package. The node produces the vector indicating the yaw and pitch that the head camera should move to to have an optimal viewing angle of the imminent collision risk location.
+
+To run the stop-to-fixate behavior, start by running the lidar-to-bump node, which produces the LiDAR "bump" sensor reading:
+```bash
+rosrun lidar_to_bump lidar_to_bump.py
+```
+Then, to produce the actual stop-to-fixate vector, run the stop-to-fixate node, which subscribes to the LiDAR reading:
+```bash
+rosrun stop_to_fixate headcam_stf.py
+```
+The actual motion is carried out by the vector_to_movement package, so launch that package next ->
+
+## Vector to Movement Package
+This package contains the functionality for movment based on the weighted viewpoint selection vector or the stop-to-fixate interrupt vector. The package decides to move either the head camera or the left arm-in-hand camera based on which is the secondary camera at the moment.
+
+To launch the actual viewpoint attainment component of our project, launch:
+```bash
+roslaunch vector_to_movement vector_to_movement.launch
+```
+
+As long as the viewpoint selection nodes are running, the robot should begin moving based on the produced vectors, or if the robot base approaches too close to an obstacle, the head camera should begin moving to observe the potential collision. 
--- camera_motion_algorithms/vector_to_movement/src/vector_to_movement.py
+++ camera_motion_algorithms/vector_to_movement/src/vector_to_movement.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-## Author: Trevor Sherrard
-## Course: Directed Research
+## Authors: Phillip Abell and Albert Enyedy
+## Course: RBE 550 Motion Planning
 ## Project: telepresence telenursing robot
-## Since: 01/30/2021
+## Since: 4/6/2021
 
 ## To use the Python MoveIt interfaces, we will import the `moveit_commander`_ namespace.
 ## This namespace provides us with a `MoveGroupCommander`_ class, a `PlanningSceneInterface`_ class,
@@ -41,13 +41,12 @@ global sec_cam
 global headcam_pitch
 global headcam_yaw
 
-global move_group1
-# move group definitions (should be unchanging)
-## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-## kinematic model and the robot's current joint states   
-group1_name = "left_arm"
-#group2_name = "right_arm"
 
+# move group definition for left arm
+global move_group1
+group1_name = "left_arm"
+
+# publishers for the head camera yaw and pitch control
 global headcam_pitch_pub
 global headcam_yaw_pub
 
@@ -75,6 +74,9 @@ def all_close(goal, actual, tolerance):
     return True
 
 def startNode():
+    """
+    Instantiate the node, its subscribers and publishers, and global variables such as the moveit commander
+    """
     global headcam_pitch_pub
     global headcam_yaw_pub
     global headcam_pitch
@@ -84,65 +86,80 @@ def startNode():
     global switchCam
     global listener
 
-    ## First initialize `moveit_commander`_ and a `rospy`_ node:
-    #moveit_commander.roscpp_initialize(sys.argv)
+    # instantiate the node
     rospy.init_node('roll_reduction', anonymous=True)
 
     # initialize head_cam pitch and yaw positions to match the default starting ones
     headcam_pitch = 0.0
     headcam_yaw = 0.0
-    sec_cam = "leftArm" # "head" for main_cam, "leftArm" for left_arm movement
+    # default secondary cam starts as left arm, values are "head" for main_cam, "leftArm" for left_arm movement
+    sec_cam = "leftArm" 
+    # set up moveit commander for left arm
     move_group1 = moveit_commander.MoveGroupCommander("left_arm")
-
+    # set up transform listener for pose transformations
     listener = tf.TransformListener()
-
-    # listener.waitForTransform("/map", "/trina2_1/base_link", rospy.Time(0), rospy.Duration(5))
-
 
     # subscribe to secondary cam topic to check which camera must be moved
     rospy.Subscriber("/secondaryCam", String, which_cam_callback, queue_size=1)
+    # publisher for overriding primary camera for stop-to-fixate interrupt
     switchCam = rospy.Publisher("/secondaryCam", String, queue_size=1)
+
     # publish the yaw and pitch to the main head camera directly (no movegroup)
     headcam_yaw_pub = rospy.Publisher('/trina2_1/main_cam_yaw_controller/command', Float64,queue_size=1)
     headcam_pitch_pub = rospy.Publisher('/trina2_1/main_cam_pitch_controller/command', Float64, queue_size=10)
 
+    # set up subscribers for the weighted viewpoint selection algorithm vector and stop-to-fixate interrupt vector
     rospy.Subscriber("/info_and_saliency_avg_pose", PoseStamped, vec_to_move, queue_size=1)
-
     rospy.Subscriber("/stop_to_fixate_vec", Vector3, stop_and_fixate, queue_size=1)
 
-
 def which_cam_callback(primary):
+    """
+    callback to update state variable of which camera is secondary camera
+    """
     global sec_cam
-    # primary = std_msgs/String
+    # primary's datatype is std_msgs/String
     sec_cam = primary.data
 
 def stop_and_fixate(vector):
+    """
+    Callback for achieving stop-to-fixate behavior.
+    Only moves head camera via interrupt when bump is detected
+    """
     global headcam_pitch_pub
     global headcam_yaw_pub
     global headcam_pitch
     global headcam_yaw
 
+    # set up messages for publishing
     pitch_msg = Float64()
     yaw_msg = Float64()
 
+    # start with current head camera pitch and yaw
     pitch_msg.data = headcam_pitch
     yaw_msg.data = headcam_yaw
 
+    # only move head if goal is > pi/6 radians difference from current head camera pitch and yaw
     if (abs(vector.y - headcam_yaw) > 3.14 / 6):
+        # update the head camera yaw in memory, then set new yaw for publishing
         headcam_yaw = vector.y
         yaw_msg.data = vector.y
-
     if (abs(vector.z - headcam_pitch) > 3.14 / 6):
+        # update the head camera pitch in memory, then set new pitch for publishing
         headcam_pitch = vector.z
         pitch_msg.data = vector.z
 
+    # publish the pitch and yaw to control the head camera directly with quick motion
     headcam_pitch_pub.publish(pitch_msg)
     headcam_yaw_pub.publish(yaw_msg)
 
+    # switch left arm to secondary camera, thus having teleoperator instantly see the obstacle after motion is complete
     switchCam.publish("leftArm")
 
-# Takes in a 3d vector, moves the secondary camera incrementally
 def vec_to_move(vector):
+    """
+    Callback from weighted viewpoint selection vector subscriber
+    Takes in the weighted 3d vector, moves the secondary camera incrementally towards the viewpoint
+    """
     global headcam_pitch_pub
     global headcam_yaw_pub
     global headcam_pitch
@@ -150,10 +167,12 @@ def vec_to_move(vector):
     global sec_cam
     global move_group1
 
+    # set goal position
     x_goal = vector.pose.position.x
     y_goal = vector.pose.position.y
     z_goal = vector.pose.position.z
-
+    
+    # set goal orientation
     o_x_goal = vector.pose.orientation.x
     o_y_goal = vector.pose.orientation.y
     o_z_goal = vector.pose.orientation.z
@@ -161,13 +180,13 @@ def vec_to_move(vector):
 
     # since only the left arm uses a movegroup for the cameras, movegroup will always be move_group1
     move_group = move_group1
+    # if left arm is secondary camera, use moveit commander to move the arm towards the vector
     if (sec_cam == "leftArm"):
-
-        #current_move_group = move_group1
+        # get current pose of arm to generate incremental move goals
         current_pose = move_group.get_current_pose()
+        # transform pose to correct frame id
         current_pose = listener.transformPose('/map', move_group.get_current_pose()).pose
-        print("current pose: ")
-        rospy.loginfo(current_pose)
+        # set current pose of arm in correct frame id
         x = current_pose.position.x
         y = current_pose.position.y
         z = current_pose.position.z
@@ -175,13 +194,15 @@ def vec_to_move(vector):
         oy = current_pose.orientation.y
         oz = current_pose.orientation.z
         ow = current_pose.orientation.w
+        
+        # convert current pose from quaternion to roll pitch yaw
         (rollRe, pitchRe, yawRe) = euler_from_quaternion([ox, oy, oz, ow])
         roll = rollRe
         pitch = pitchRe
         yaw = yawRe
 
-        # increment the motion for the goal based on the input vector
-        movescale = 0.05 # formerly 0.05
+        # increment the motion for the goal based on the input vector, using small scale
+        movescale = 0.05 
 
         # maintain the same orientation to begin with
         pose_goal = geometry_msgs.msg.Pose()
@@ -194,24 +215,17 @@ def vec_to_move(vector):
         pose_goal.position.x = x - (x_goal * movescale)
         pose_goal.position.y = y - (y_goal * movescale)
         pose_goal.position.z = z - (z_goal * movescale)
-        
-        print("pose_goal: ")
-        rospy.loginfo(pose_goal)
 
         # execute the planned motion
         ps = geometry_msgs.msg.PoseStamped()
         ps.header.frame_id = '/map'
         ps.pose = pose_goal
         ps = listener.transformPose('/trina2_1/base_link', ps)
-
         pose_goal = ps.pose
-
-        print("transformed goal")
-        rospy.loginfo(ps)
         move_group.set_pose_target(pose_goal)
         move_group.set_goal_tolerance = 0.08
         
-        
+        # move incrementally in the vector's direction until close enough to the incremental goal
         while True:
             plan = move_group.go(wait=True)
             current_pose = move_group.get_current_pose().pose
@@ -222,11 +236,11 @@ def vec_to_move(vector):
         move_group.stop()
         move_group.clear_pose_targets()
 
+    # use the head camera to achieve the viewpoint 
     else:
         # publish to "/trina2_1/main_cam_yaw_controller/command" (type Float64) for yaw control
         # publish to "/trina2_1/main_cam_pitch_controller/command" (type Float64) for pitch control
-        print("head_cam is selected")
-
+        
         # set up the control messages (starting at current pitch and yaw)
         pitch_msg = Float64()
         pitch_msg.data = headcam_pitch
@@ -247,41 +261,19 @@ def vec_to_move(vector):
             pitch_msg.data -= 0.1
             headcam_pitch -= 0.1
 
+        # publish the move goals to move the head camera directly
         headcam_pitch_pub.publish(pitch_msg)
         headcam_yaw_pub.publish(yaw_msg)
 
-
-
 if __name__ == "__main__":
+    """
+    initialize the node then spin to perform desired motions based on the vector callbacks
+    """
     global headcam_pitch_pub
     global headcam_yaw_pub
     global sec_cam
+    # initializes the node and instantiates default variable values
     startNode()
-    move_vector = [5,0,0]
-
-    #vec_to_move([5, 5, 5])
-    # rostopic pub /trina2_1/main_cam_pitch_controller/command std_msgs/Float64 20
-    # pitch_msg = Float64()
-    # pitch_msg.data = 0.0
-    # yaw_msg = Float64()
-    # yaw_msg.data = 0.0
-    # # headcam_pitch_pub.publish(pitch_msg)
-    # rospy.loginfo(pitch_msg)
-    # rospy.loginfo(" we are publishing pitch")
+    # spin to update the callback functions and perform the motion towards the received vector
     rospy.spin()
-    # while not rospy.is_shutdown():
-        
-    #     vec_to_move(move_vector)
-        
-        # if(pitch_msg.data < 60.0):
-        #     pitch_msg.data += 0.1
-        # if(yaw_msg.data < 60.0):
-        #     yaw_msg.data += 0.1
-
-        # #headcam_pitch_pub.publish(pitch_msg)
-        # headcam_yaw_pub.publish(yaw_msg)
-        # #rospy.loginfo(pitch_msg)
-        # rospy.loginfo(yaw_msg)
-        # #rospy.loginfo(" we are publishing pitch")
-        # rospy.loginfo(" we are publishing yaw")
 
